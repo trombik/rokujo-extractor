ab6ec12b28c27b3010a71d223161182c5c52f363
Tomoyuki Sakurai <y@trombik.org>
2025-12-28 21:05:25 +0700

    feat: implements JSONL extractor

---
+++ lib/rokujo/extractor/jsonl.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "json"
+
+module Rokujo
+  module Extractor
+    # Extract sentences from .jsonl file
+    #
+    # It assumes that the text is the value of "text" key.
+    class JSONL < Base
+      protected
+
+      def raw_text
+        texts = []
+        file_content.each_line do |line|
+          texts << JSON.parse(line)["text"]
+        end
+        texts.join("\n")
+      end
+
+      def file_content
+        File.read(@file_path)
+      end
+    end
+  end
+end
