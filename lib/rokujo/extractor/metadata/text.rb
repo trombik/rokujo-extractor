bee9532317e71bf7652d85ee862d6a8717824502
Tomoyuki Sakurai <y@trombik.org>
2025-12-31 14:14:43 +0700

    feat: Introduce `Rokujo::Extractor::Metadata::Text` for extracting metadata from text file
    Note: Windows support for `birthtime` is a TODO.

---
+++ lib/rokujo/extractor/metadata/text.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "uri"
+require "time"
+require "pathname"
+
+module Rokujo
+  module Extractor
+    module Metadata
+      # Metadata for text file on local file system.
+      class Text < Base
+        attr_reader :acquired_at, :location
+
+        include Rokujo::Extractor::Concerns::Identifiable
+
+        def initialize(location, opts = {})
+          super
+          @location = Pathname.new(location)
+          doc
+        end
+
+        def title
+          location.basename(".*").to_s
+        end
+
+        def author
+          nil
+        end
+
+        def uri
+          URI::File.build([nil, location.realpath]).to_s
+        end
+
+        def created_at
+          # TODO: support Windows (use ctime instead of birthtime)
+          File.stat(location).birthtime
+        rescue NotImplementedError
+          updated_at
+        end
+
+        def updated_at
+          File.stat(location).mtime
+        end
+
+        def to_h
+        end
+
+        private
+
+        def doc
+          @doc ||= ::File.read(location)
+          @acquired_at = Time.now.utc.iso8601
+        end
+      end
+    end
+  end
+end
+
---
+++ spec/rokujo/extractor/metadata/text_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Rokujo::Extractor::Metadata::Text do
+  let(:metadata) { described_class.new(path) }
+  let(:path) { Pathname.new "/bar/foo.txt" }
+
+  before do
+    allow(File).to receive(:read).and_return("some contents")
+    location = instance_double(Pathname)
+    allow(location).to receive(:realpath).and_return(path.to_s)
+    allow(location).to receive(:basename).and_return("foo")
+    allow(metadata).to receive(:location).and_return(location)
+  end
+
+  describe "#new" do
+    it "does not raise" do
+      expect { metadata }.not_to raise_error
+    end
+  end
+
+  describe "#title" do
+    it "returns file name without extention" do
+      expect(metadata.title).to eq "foo"
+    end
+  end
+
+  describe "#author" do
+    it "returns nil" do
+      expect(metadata.author).to be nil
+    end
+  end
+
+  describe "#uri" do
+    it "returns a file URI" do
+      expect(metadata.uri).to eq "file://#{path}"
+    end
+  end
+
+  describe "#created_at" do
+    it "returns birthtime" do
+      now = Time.now.utc.iso8601
+      stat = instance_double("File::Stat")
+      allow(stat).to receive(:birthtime).and_return now
+      allow(File).to receive(:stat).and_return(stat)
+
+      expect(metadata.created_at).to eq now
+    end
+  end
+
+  describe "#updated_at" do
+    it "returns mtime" do
+      now = Time.now.utc.iso8601
+      stat = instance_double("File::Stat")
+      allow(stat).to receive(:mtime).and_return now
+      allow(File).to receive(:stat).and_return(stat)
+
+      expect(metadata.updated_at).to eq now
+    end
+  end
+
+  describe "#to_h" do
+    it "returns metadata as a Hash" do
+      pending "Not implemented yet"
+      expect(metadata.to_h).to be_a Hash
+    end
+  end
+end
