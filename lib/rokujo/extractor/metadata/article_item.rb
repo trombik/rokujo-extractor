bf59989b61a00cdbea4bd1f99ef21ace4a516f24
Tomoyuki Sakurai <y@trombik.org>
2026-01-17 10:40:52 +0700

    feat: Implement Metadata::ArticleItem

---
+++ lib/rokujo/extractor/metadata/article_item.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "json"
+
+require_relative "base"
+
+module Rokujo
+  module Extractor
+    module Metadata
+      # Metadata for ArticleItem.
+      class ArticleItem < Base
+        attr_reader :json
+
+        def initialize(json_string, opts = {})
+          # as the metadata comes from json_string, pass an empty string for
+          # location.
+          super("", opts)
+          @json = JSON.parse(json_string)
+        end
+
+        def title
+          json["title"]
+        end
+
+        def author
+          json["author"]
+        end
+
+        def uri
+          json["url"]
+        end
+
+        def created_at
+          json["published_time"]
+        end
+
+        def updated_at
+          json["modified_time"]
+        end
+
+        def acquired_at
+          json["acquired_time"]
+        end
+
+        def extra_attributes
+          {
+            description: json["description"],
+            site_name: json["site_name"],
+            kind: json["kind"]
+          }
+        end
+      end
+    end
+  end
+end
